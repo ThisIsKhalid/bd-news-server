294fe72a6b73f36b8de7ec0d25e47bafebf8007a
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-10-20 23:57:10 +0600

    added news

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
+const cors = require("cors");
 
-const categories = require('./data/categories.json');
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('News api running');
+app.get("/", (req, res) => {
+  res.send("News api running");
 });
 
-app.get('/news-categories', (req, res) => {
-    res.send(categories);
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);      
+    }
 })
 
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+// dynamic vabe data load, single id diye oi id er data pawa jabe
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
 
 app.listen(port, () => {
-    console.log('Dragon News Server running on port', port);
+  console.log("Dragon News Server running on port", port);
 });
